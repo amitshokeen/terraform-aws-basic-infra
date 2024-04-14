ab466c80a989c47f645db67c6e55f0433b35367f
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2024-04-14 12:11:24 +1000

    aws provider

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+  alias = "sydney"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias = "mumbai"
+}
