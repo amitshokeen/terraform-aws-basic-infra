51fc5389121f0339d7c7d67ac92dc1d447345d78
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2024-04-19 19:14:41 +1000

    updated outputs

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+module "my_vpc" {
+  source = "./modules/vpc"
+}
+
 module "my_ec2_instance" {
   source              = "./modules/ec2-instance"
   ec2_instance_type   = var.ec2_instance_type
@@ -10,4 +14,20 @@ module "my_ec2_instance" {
 
 output "ec2_instance_id" {
   value = module.my_ec2_instance.ec2_instance_id
+}
+
+output "ec2_public_ipv4_address" {
+  value = module.my_ec2_instance.ec2_instance_public_ipv4
+}
+
+output "vpc_id" {
+  value = module.my_vpc.vpc_id
+}
+
+output "public_subnet_id" {
+  value = module.my_vpc.public_subnet_id
+}
+
+output "vpc_sg_id" {
+  value = module.my_vpc.vpc_sg_id
 }
--- modules/ec2-instance/outputs.tf
+++ modules/ec2-instance/outputs.tf
@@ -2,3 +2,6 @@ output "ec2_instance_id" {
   value = aws_instance.sample[*].id
 }
 
+output "ec2_instance_public_ipv4" {
+  value = aws_instance.sample[*].public_ip
+}
